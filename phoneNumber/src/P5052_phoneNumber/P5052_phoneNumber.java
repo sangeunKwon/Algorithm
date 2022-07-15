e292608c2dab0a658a09c3734a5d72c5a3298851
Sangeun Kwon <ksg95c@gmail.com>
2022-07-15 14:33:27 +0900

    Rename phoneNumber to P5052_phoneNumber

--- phoneNumber/src/P5052_phoneNumber/P5052_phoneNumber.java
+++ phoneNumber/src/P5052_phoneNumber/P5052_phoneNumber.java
@@ -1,11 +1,11 @@
-package phoneNumber;
+package P5052_phoneNumber;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
 
-public class phoneNumber {
+public class P5052_phoneNumber {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int testCase = Integer.parseInt(br.readLine()); //테스트 케이스 개수
